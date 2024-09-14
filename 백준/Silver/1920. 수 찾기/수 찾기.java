33a8323fc6ca179e6f020fda6d23640b118baac1
mini <155148431+mini0192@users.noreply.github.com>
2024-09-14 21:19:32 +0900

    [Silver IV] Title: 수 찾기, Time: 580 ms, Memory: 45764 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,13 +5,12 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Main {
-    private static int[] line;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
 
         int N = Integer.parseInt(br.readLine());
-        line = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] line = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         int M = Integer.parseInt(br.readLine());
         int[] check = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -19,22 +18,23 @@ public class Main {
         Arrays.sort(line);
 
         for(int c : check) {
-            if(check(0, line.length - 1, c)) sb.append("1\n");
+            if(check(line, c)) sb.append("1\n");
             else sb.append("0\n");
         }
 
         System.out.println(sb);
     }
 
-    private static boolean check(int start, int end, int number) {
-        if(line[start] == number || line[end] == number) return true;
+    private static boolean check(int[] line, int number) {
+        int start = 0;
+        int end = line.length - 1;
 
-        int mid = (start + end) / 2;
-        if(mid == start || mid == end) return false;
-
-        if(number == line[mid]) return true;
-        if(number < line[mid]) return check(start, mid, number);
-        if(number > line[mid]) return check(mid, end, number);
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(line[mid] == number) return true;
+            else if(line[mid] > number) end = mid - 1;
+            else if(line[mid] < number) start = mid + 1;
+        }
 
         return false;
     }
