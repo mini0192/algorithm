7a5d1e92947e500149f86309a069b1305855f4f6
mini <parkgw9071@gmail.com>
2025-03-27 15:08:02 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            String category = cloth[1];
+            map.put(category, map.getOrDefault(category, 0) + 1);
+        }
+
+        int answer = 1;
+        for (int count : map.values()) {
+            answer *= (count + 1);
+        }
+
+        return answer - 1;
+    }
+}
